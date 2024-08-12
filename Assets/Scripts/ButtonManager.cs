7bb42a261d4f676a7e26af707d15759fd171782f
kavikr3 <kavi@magesstudio.com.sg>
2024-08-12 22:01:07 +0800

    Added sounds for Milestone 1

--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -9,9 +9,23 @@ using UnityEngine.XR.ARFoundation;
 public class ButtonManager : HubButton
 {
     public UnityEvent eventOnButtonClick;
+    public AudioSource buttonClickAudio;
 
     public override void Press(){
+
+        if(buttonClickAudio != null){
+            buttonClickAudio.Play();
+        }
+
+        StartCoroutine(InvokeEvent());
+
+    }
+
+    IEnumerator InvokeEvent(){
+        yield return new WaitForSeconds(buttonClickAudio.clip.length);
         eventOnButtonClick.Invoke();
+
+        }
     }
    
-}
+
--- Assets/Scripts/LogoShaderController.cs
+++ Assets/Scripts/LogoShaderController.cs
@@ -23,30 +23,39 @@ private Coroutine thresholdCoroutine;
     {
         if(material.HasProperty(thresholdProperty)){
             thresholdCoroutine = StartCoroutine(ChangeThresholdOverTime(startThreshold, endThreshold, duration));
-
-            if(audioSource != null){
-                audioSource.Play();
-            }
+            Invoke("PlayAudio",2); //Little hardcoded to wait for the model to load first //Can work on it later to get a proper workaround
         }
         else{
             Debug.Log("No such property in material found!");
         }
     }
 
+    public void PlayAudio(){
+                if(audioSource != null){
+                audioSource.Play();
+            }
+    }
+
     IEnumerator ChangeThresholdOverTime(float startValue, float endValue, float duration)
     {
+        
         float elapsedTime = 0f;
-
         while (elapsedTime < duration)
         {
             elapsedTime += Time.deltaTime;
+            
+            
             float newThreshold = Mathf.Lerp(startValue, endValue, elapsedTime / duration);
 
             // Set the threshold property on the material
             material.SetFloat(thresholdProperty, newThreshold);
 
             yield return null; // Wait for the next frame
+            
+            
         }
+
+        
 
         // Ensure the final value is set
         material.SetFloat(thresholdProperty, endValue);
